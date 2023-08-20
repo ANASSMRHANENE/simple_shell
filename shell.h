d45176c66755b96c9481692632c7c596a94c46ac
ANASSMRHANENE <mrhanene@gmail.com>
2023-08-20 15:09:35 -0700

    my commit

--- shell.h
+++ shell.h
@@ -1,27 +1,26 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/*---LIBRARIES---*/
+/*LIBRARIES*/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 
-/*---Macros---*/
 #define TOK_DELIM " \t\r\n\a\""
 extern char **environ;
 
-/*---FUNCTION PROTOTYPES---*/
-/* main.c */
+/*FUNCTION PROTOTYPES*/
+/*main.c*/
 /*interactive and no_interactive mode functions*/
 void interactive(void);
 void no_interactive(void);
 
 /* shell_interactive.c */
-/*function to read user input*/
+/*function to read the command input*/
 char *read_cmd(void);
-/*function to split input string*/
+/*function to split the command input*/
 char **split_cmd(char *line);
 int execute_args(char **args);
 
@@ -32,7 +31,7 @@ int new_process(char **args);
 /*function to read from file stream*/
 char *read_file(void);
 
-/*---Builtin func---*/
+/*Builtin func*/
 int own_cd(char **args);
 int own_exit(char **args);
 int own_env(char **args);
