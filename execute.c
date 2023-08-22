684687f0eef2e5f295e40dd20293f93c0fcd3aab
ANASSMRHANENE <mrhanene@gmail.com>
2023-08-22 15:49:13 -0700

    my commit

--- execute.c
+++ execute.c
@@ -8,9 +8,9 @@
 int execute_cmd(char **agmts)
 {
 	long unsigned int n = 0;
-	char *built_in_cmdlist[] = {"exit"};
+	char *builtin_cmdlist[] = {"exit"};
 
-	int (*built_in_cmd[])(char **) = {&my_exit};
+	int (*builtin_cmd[])(char **) = {&my_exit};
 
 	if (agmts[0] == NULL)
 	{
@@ -19,12 +19,12 @@ int execute_cmd(char **agmts)
 	}
 
 	/*determine if a command is builtin or not*/
-	for (; n < sizeof(built_in_cmdlist) / sizeof(char *); n++)
+	for (; n < sizeof(builtin_cmdlist) / sizeof(char *); n++)
 	{
 		/*execute the builtin command*/
-		if (strcmp(agmts[0], built_in_cmdlist[n]) == 0)
+		if (strcmp(agmts[0], builtin_cmdlist[n]) == 0)
 		{
-			return ((*built_in_cmd[n])(agmts));
+			return ((*builtin_cmd[n])(agmts));
 		}
 	}
 	/*fork a process*/
